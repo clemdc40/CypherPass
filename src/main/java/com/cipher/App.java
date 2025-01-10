2c2020bff2af66771c59f209cdebb05f6f64dbe5
clemdc40 <clementdacruz10@gmail.com>
2025-01-10 22:09:36 +0100

    Add login function

--- src/main/java/com/cipher/App.java
+++ src/main/java/com/cipher/App.java
@@ -1,12 +1,17 @@
 package com.cipher;
 
+import java.util.Scanner;
+
 import com.cipher.Outils.Chiffrement;
 
 public class App {
     public static void main(String[] args) {
         Chiffrement chiffrement = new Chiffrement(true);
 
-        String texteClair = "Azertyuiop40/";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez votre mot de passe : ");
+        String texteClair = scanner.nextLine();
+
         String texteChiffre = chiffrement.chiffrer(texteClair);
         System.out.println("Texte chiffré : " + texteChiffre);
 
@@ -19,5 +24,32 @@ public class App {
         } catch (Exception e) {
             System.err.println("Erreur lors de la sauvegarde de la clé : " + e.getMessage());
         }
+
+        try {
+            chiffrement.savePassword(texteChiffre);
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la sauvegarde du mot de passe : " + e.getMessage());
+        }        
+
+        Boolean passwordMatch = false;
+        while (!passwordMatch) {
+            System.out.print("Entrez votre mot de passe pour vérifier : ");
+            String inputPassword = scanner.nextLine();
+            String encryptedInputPassword = chiffrement.chiffrer(inputPassword);
+
+            try {
+            passwordMatch = chiffrement.connectPassword(encryptedInputPassword);
+            if (passwordMatch) {
+                System.out.println("Mot de passe correct.");
+            } else {
+                System.out.println("Mot de passe incorrect. Veuillez réessayer.");
+            }
+            } catch (Exception e) {
+            System.err.println("Erreur lors de la vérification du mot de passe : " + e.getMessage());
+            }
+        }
+
+
+        scanner.close();
     }
 }
